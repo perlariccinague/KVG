df2b38b6e7abeb927a4a292a4f07039662751c43
PN <pn@klapproth-koch.de>
2024-03-07 10:00:50 +0100

    Mobile Stylsheets, footer...

--- templates/footer.php
+++ templates/footer.php
@@ -1,15 +1,22 @@
-<div class="map-icon">
-    <a href="https://www.google.com/maps/place/Dr.+med.+Volker+Gr%C3%A4fe/@50.9820304,11.3217023,17z/data=!3m2!4b1!5s0x47a41ac3c2a507e5:0x96687ddb04850e77!4m6!3m5!1s0x47a41ac3e171f90d:0xbca28ec94a8195fb!8m2!3d50.9820271!4d11.3242772!16s%2Fg%2F1vhq2qhm?entry=ttu" target="_blank">
-        <!--<img src="files/assets/layout/Location.svg" alt="location">-->
+<!--<div class="map-icon">
+    <a href="https://www.google.com/maps/place/Carl-August-Allee+1,+99423+Weimar/@50.9873209,11.3211913,17z/data=!3m1!4b1!4m6!3m5!1s0x47a41aea1fbe7b27:0x81ae254784ddd09b!8m2!3d50.9873176!4d11.3260622!16s%2Fg%2F11bw3_n5nr?entry=ttu" target="_blank">
         <svg  class="marker" xmlns="http://www.w3.org/2000/svg" width="35.668" height="42.429" viewBox="0 0 35.668 42.429">
             <path id="Location" d="M2.153,2.888A6.276,6.276,0,0,1-4.122-3.387,6.276,6.276,0,0,1,2.153-9.663,6.276,6.276,0,0,1,8.429-3.387,6.276,6.276,0,0,1,2.153,2.888M14.751-15.8a17.6,17.6,0,0,0-12.6-5.245,17.744,17.744,0,0,0-12.6,5.2A17.786,17.786,0,0,0-15.69-3.2a17.831,17.831,0,0,0,5.2,12.6L1.076,20.964a1.373,1.373,0,0,0,1.03.422,1.306,1.306,0,0,0,1.03-.422L14.7,9.4a17.737,17.737,0,0,0,.047-25.2" transform="translate(15.69 21.043)" fill="currentColor"/>
         </svg>
     </a>
-</div>
+</div>-->
 <div class="footer desktop-only">
     <div class="footer-left">
         {{insert_content::44}}
         {{insert_content::47}}
+        <div class="map-icon">
+            <a href="https://www.google.com/maps/place/Carl-August-Allee+1,+99423+Weimar/@50.9873209,11.3211913,17z/data=!3m1!4b1!4m6!3m5!1s0x47a41aea1fbe7b27:0x81ae254784ddd09b!8m2!3d50.9873176!4d11.3260622!16s%2Fg%2F11bw3_n5nr?entry=ttu" target="_blank">
+                <svg  class="marker" xmlns="http://www.w3.org/2000/svg" width="35.668" height="42.429" viewBox="0 0 35.668 42.429">
+                    <path id="Location" d="M2.153,2.888A6.276,6.276,0,0,1-4.122-3.387,6.276,6.276,0,0,1,2.153-9.663,6.276,6.276,0,0,1,8.429-3.387,6.276,6.276,0,0,1,2.153,2.888M14.751-15.8a17.6,17.6,0,0,0-12.6-5.245,17.744,17.744,0,0,0-12.6,5.2A17.786,17.786,0,0,0-15.69-3.2a17.831,17.831,0,0,0,5.2,12.6L1.076,20.964a1.373,1.373,0,0,0,1.03.422,1.306,1.306,0,0,0,1.03-.422L14.7,9.4a17.737,17.737,0,0,0,.047-25.2" transform="translate(15.69 21.043)" fill="currentColor"/>
+                </svg>
+                <span>Anfahrt zu Google Maps </span>
+            </a>
+        </div>
         {{insert_module::4}}
     </div>
     <div class="footer-right">
@@ -22,7 +29,14 @@
     <div class="footer-left">
         {{insert_content::44}}
         {{insert_content::47}}
-       <!-- {{insert_content::45}}-->
+        <div class="map-icon">
+            <a href="https://www.google.com/maps/place/Carl-August-Allee+1,+99423+Weimar/@50.9873209,11.3211913,17z/data=!3m1!4b1!4m6!3m5!1s0x47a41aea1fbe7b27:0x81ae254784ddd09b!8m2!3d50.9873176!4d11.3260622!16s%2Fg%2F11bw3_n5nr?entry=ttu" target="_blank">
+                <svg  class="marker" xmlns="http://www.w3.org/2000/svg" width="35.668" height="42.429" viewBox="0 0 35.668 42.429">
+                    <path id="Location" d="M2.153,2.888A6.276,6.276,0,0,1-4.122-3.387,6.276,6.276,0,0,1,2.153-9.663,6.276,6.276,0,0,1,8.429-3.387,6.276,6.276,0,0,1,2.153,2.888M14.751-15.8a17.6,17.6,0,0,0-12.6-5.245,17.744,17.744,0,0,0-12.6,5.2A17.786,17.786,0,0,0-15.69-3.2a17.831,17.831,0,0,0,5.2,12.6L1.076,20.964a1.373,1.373,0,0,0,1.03.422,1.306,1.306,0,0,0,1.03-.422L14.7,9.4a17.737,17.737,0,0,0,.047-25.2" transform="translate(15.69 21.043)" fill="currentColor"/>
+                </svg>
+                <span>Anfahrt zu Google Maps </span>
+            </a>
+        </div>
         {{insert_module::4}}
     </div>
     <!--<div class="footer-right">
